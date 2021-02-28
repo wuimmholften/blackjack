e9abc657ea21bef2d902a5b64e7dc41b0d745c19
WuimmHoften <erickgarciavalenzuela@hotmail.com>
2021-02-28 01:47:24 -0700

    Preparando la logica y los TODO del juego.

--- Program.cs
+++ Program.cs
@@ -4,9 +4,25 @@ namespace Blackjack
 {
     class Program
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            // TODO 1: El jugador comienza con un conjunto de cartas vacío
+            // TODO 2: Preguntar al jugador si desea quedarse con las cartas que tiene, o si solicita una carta
+            // TODO 3: Si el jugador solicita una carta, generar aleatoriamente una carta de una baraja y agregarla a las cartas que tiene el jugador.
+            // TODO 4: Calcular el valor de las cartas que tiene el jugador (numérico) y mostrarlo cada vez que el jugador obtenga una carta. Considerar el valor del as como 11.
+            // TODO 5: Volver a preguntar al jugador lo mismo que en el paso (2), hasta que el jugador decida detenerse o hasta que se pase de 21.
+            // TODO 6: Cuando el jugador se detenga, generar aleatoriamente un número entre el 17 y el 26 (incluyendo ambos números). Este será el valor de "la casa" contra el cual debe competir el número obtenido por las cartas del jugador.
+            // TODO 7: Condicion de victoria
+            // TODO 8: Comparar ambos números mostrados para determinar el ganador, y mostrar el resultado
+            // TODO 9: Modificar el valor del as; tomarlo con valor de 11 cuando sea posible hacerlo sin que el número total del jugador sobrepase el límite de 21, o en caso contrario tomarlo con valor de 1
+            // TODO 10: Modificar la lógica de inicio de juego para entregar la primer tarjeta al jugador automáticamente sin que lo solicite
+
         }
     }
 }
