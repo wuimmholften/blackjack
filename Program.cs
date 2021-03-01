fb39d3496563ea1a70054342e07ac646b360eae4
WuimmHoften <erickgarciavalenzuela@hotmail.com>
2021-03-01 00:13:54 -0700

    Se crean las variables con las que se interpretaran como los valores de las cartas.

--- Program.cs
+++ Program.cs
@@ -5,24 +5,30 @@ namespace Blackjack
     class Program
     {
         /// <summary>
-        /// 
+        /// Se solicita dos opcion al jugar para jugar
         /// </summary>
-        /// <param name="args"></param>
+        /// <param name="JugadorValorCartas">Determina el valor de las cartas del jugador</param>
+        /// <param name="ComputadoraValorCartas">Es donde se almacenara el valor de la computadora</param>
         
         static void Main(string[] args)
         {
 
             // TODO 1: El jugador comienza con un conjunto de cartas vacío
-            // TODO 2: Preguntar al jugador si desea quedarse con las cartas que tiene, o si solicita una carta
-            // TODO 3: Si el jugador solicita una carta, generar aleatoriamente una carta de una baraja y agregarla a las cartas que tiene el jugador.
-            // TODO 4: Calcular el valor de las cartas que tiene el jugador (numérico) y mostrarlo cada vez que el jugador obtenga una carta. Considerar el valor del as como 11.
-            // TODO 5: Volver a preguntar al jugador lo mismo que en el paso (2), hasta que el jugador decida detenerse o hasta que se pase de 21.
-            // TODO 6: Cuando el jugador se detenga, generar aleatoriamente un número entre el 17 y el 26 (incluyendo ambos números). Este será el valor de "la casa" contra el cual debe competir el número obtenido por las cartas del jugador.
-            // TODO 7: Condicion de victoria
-            // TODO 8: Comparar ambos números mostrados para determinar el ganador, y mostrar el resultado
-            // TODO 9: Modificar el valor del as; tomarlo con valor de 11 cuando sea posible hacerlo sin que el número total del jugador sobrepase el límite de 21, o en caso contrario tomarlo con valor de 1
-            // TODO 10: Modificar la lógica de inicio de juego para entregar la primer tarjeta al jugador automáticamente sin que lo solicite
+            int JugadorValorCartas = 0;
+            int ComputadoraValorCartas = 0;
 
+            Random random = new Random();
+            //Se despliegan las cartas para el jugador
+            JugadorValorCartas += random.Next(1,12); //Numero aleatorio de 1 - 11
+            JugadorValorCartas += random.Next(1,12); //Numero aleatorio de 1 - 11
+
+            if(JugadorValorCartas > 21)
+            {
+                JugadorValorCartas -= 10;
+            }
+
+            
+            
         }
     }
 }
